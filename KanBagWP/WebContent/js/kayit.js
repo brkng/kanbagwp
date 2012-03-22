fb68b62c6ee0903003b9a936b36a956e9c971dbb
faridmovsumov@gmail.com <faridmovsumov@gmail.com@7308bfc0-0b70-9ec7-2fbf-61af9aba3e04>
2012-03-22 17:18:53 +0000

    Kayit.js ekledim kayit sayfası ekranı için

---
+++ KanBagWP/WebContent/js/kayit.js
@@ -0,0 +1,47 @@
+	function deneme1()
+	{
+		var myWin= new Ext.Window({
+			height		:	300,
+			title		:	'Kayıt İşlemi',
+			width		:	260,
+			layout		:	'form',
+			bodyStyle	:	'padding : 10px',
+		    buttonAlign : 'center',
+			labelWidth	:	80,
+			defaultType	:	'field',
+			items		:	[{
+				fieldLabel	:	'Ad',
+				width		:	130
+			},{
+				fieldLabel	:	'TC Kimlik No',
+				width		:	130
+			},{
+				fieldLabel	:	'Telefon',
+				width		:	130
+			},{
+				xtype		:	'combo',
+				fieldLabel	:	'Kan Grubu',
+				width		:	50,
+				store		:	['0 Rh (+)','0 Rh (-)','A Rh (+)','A Rh (-)','B Rh (+)','B Rh (-)','AB Rh (+)','AB Rh (-)']
+			},{
+				fieldLabel	:	'E-posta',
+				width		:	130
+			},{
+				fieldLabel	:	'Adres',
+				xtype		:	'textarea',
+				width		:	130,
+			}],
+			buttons			:	[{
+				xtype		:	'button',
+				text		:	'Kaydol',
+				handler		:	function(btn){
+					Ext.MessageBox.alert('','Kayıt İşlemi Tamamlandı');
+				}
+			}]
+		});
+		
+		myWin.show();
+		
+	}
+	
+	Ext.onReady(deneme1);
