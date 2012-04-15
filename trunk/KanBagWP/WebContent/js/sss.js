26d31024f6527c653a6c62c7bb11ec6dcb004fc7
faridmovsumov@gmail.com <faridmovsumov@gmail.com@7308bfc0-0b70-9ec7-2fbf-61af9aba3e04>
2012-04-15 19:54:30 +0000

    anasayfa.js dosyasinin çok büyümesini engellemek amacıyla sssPanel farklı dosyaya alındı sss.js

---
+++ trunk/KanBagWP/WebContent/js/sss.js
@@ -0,0 +1,81 @@
+				var sssPanel = new Ext.Panel({
+					
+            	title: 'Sıkça Sorulan Sorular',
+            	closeAction: 'hide',
+            	id:'sss',
+            	bodyStyle:'padding:10px;font-size:12px;background-color:#E6E6E2;',
+            	items : [
+            	{
+            		title: 'Çok canım yanar mı?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Kullanılan iğnenin kalınlığı uluslararası standartlarda bu işlem için uygulanan kalınlıktadır. Hayat kurtarmak için alınan kanın içindeki hücrelere zarar verilmemesi açısından iğnenin kalın olması gerekmektedir. İğnenin kalın olması kan alımı sırasında iğne içinden geçen kan hücrelerinin parçalanmasını engeller, oluşabilecek hasarı azaltır. Acının az hissedilmesi için iğne ucu özel bir işlemle lazerle kesilmiş, silikon ile kaplanmıştır. Personelimiz kan alımı konusunda özel eğitim almış uzmanlardır. Hissettiğiniz acı çok azdır.'
+            	},
+            	{
+            		title: 'Kimller kan bağışlayabilir?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : '18-65 Yaş arasında, ağırlığı en az 50 kg ve üzerinde olan her sağlıklı birey kan bağışında bulunabilir. '
+            	},
+            	{
+            		title: 'Kan bağışı ne kadar sürer?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Kayıt, muayene, kan verme ve ikram işlemlerinin tamamı yaklaşık olarak 30-35 dakika sürmektedir. Bir hayat kurtarılması için bu süreyi gözden çıkarmaya değmez mi? '
+            	},
+            	{
+            		title: 'korkuyorum. Kan bağışlayabilir miyim?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Eğer korkunuz sizi bayıltacak kadar çok ise kan vermeniz uygun değildir. Bir yaşam kurtarma düşüncesi, bütün korkularınızı yener. '
+            	},
+            	{
+            		title: 'Tarama test sonuçlarım bana bildirilecek mi?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Eğer sonuçlarınız negatif bulunmuşsa size herhangi bir bildirimde bulunulmayacaktır. '
+            	},
+            	{
+            		title: 'Tarama test sonuçlarım bana bildirilecek mi?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Eğer sonuçlarınız negatif bulunmuşsa size herhangi bir bildirimde bulunulmayacaktır. '
+            	},
+            	{
+            		title: 'Çok canım yanar mı?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Kullanılan iğnenin kalınlığı uluslararası standartlarda bu işlem için uygulanan kalınlıktadır. Hayat kurtarmak için alınan kanın içindeki hücrelere zarar verilmemesi açısından iğnenin kalın olması gerekmektedir. İğnenin kalın olması kan alımı sırasında iğne içinden geçen kan hücrelerinin parçalanmasını engeller, oluşabilecek hasarı azaltır. Acının az hissedilmesi için iğne ucu özel bir işlemle lazerle kesilmiş, silikon ile kaplanmıştır. Personelimiz kan alımı konusunda özel eğitim almış uzmanlardır. Hissettiğiniz acı çok azdır.'
+            	},
+            	{
+            		title: 'Kimller kan bağışlayabilir?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : '18-65 Yaş arasında, ağırlığı en az 50 kg ve üzerinde olan her sağlıklı birey kan bağışında bulunabilir. '
+            	},
+            	{
+            		title: 'Kan bağışı ne kadar sürer?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Kayıt, muayene, kan verme ve ikram işlemlerinin tamamı yaklaşık olarak 30-35 dakika sürmektedir. Bir hayat kurtarılması için bu süreyi gözden çıkarmaya değmez mi? '
+            	},
+            	{
+            		title: 'korkuyorum. Kan bağışlayabilir miyim?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Eğer korkunuz sizi bayıltacak kadar çok ise kan vermeniz uygun değildir. Bir yaşam kurtarma düşüncesi, bütün korkularınızı yener. '
+            	},
+            	{
+            		title: 'Tarama test sonuçlarım bana bildirilecek mi?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Eğer sonuçlarınız negatif bulunmuşsa size herhangi bir bildirimde bulunulmayacaktır. '
+            	},
+            	{
+            		title: 'Tarama test sonuçlarım bana bildirilecek mi?',
+                	collapsed: true,
+               		collapsible	:true,
+               		html : 'Eğer sonuçlarınız negatif bulunmuşsa size herhangi bir bildirimde bulunulmayacaktır. '
+            	}
+            	]
+            });
